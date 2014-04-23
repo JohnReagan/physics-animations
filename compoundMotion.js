15c57903e2edf8f43cbc387961c870b0914a3cd7
jtr4fh <jtr4fh@virginia.edu>
2014-04-23 02:19:31 -0400

    Completed functionality of compoundMotion app.

--- compoundMotion.js
+++ compoundMotion.js
@@ -7,12 +7,10 @@ var startButton = document.getElementById('startAnimation');
 var stopButton = document.getElementById('stopAnimation');
 
 $(startButton).click(function() {
-	playAnimation = true;
-	animate();
-});
-
-$(stopButton).click(function() {
-	playAnimation = false;
+	//play 3 times and stop
+	playLoop();
+	setTimeout(playLoop, 3500);
+	setTimeout(playLoop, 7000);
 });
 
 var Ball = function(x, y, r, vX, vY, aY) {
@@ -25,7 +23,9 @@ var Ball = function(x, y, r, vX, vY, aY) {
 }
 
 var balls = new Array();
-//var myBall = new Ball(422, 38, 10, -5, 0, .1);
+var drawPoints = new Array(); //array of points on projectile path
+var count = 0; //count for parabola points
+
 balls[0] = new Ball(422, 38, 10, -5, 0, .1);
 balls[1] = new Ball(422, 38, 10, -5, 0, 0);
 balls[2] = new Ball(422, 38, 10, 0, 0, .1);
@@ -33,6 +33,13 @@ balls[2] = new Ball(422, 38, 10, 0, 0, .1);
 function animate() {
 	context.clearRect(0,0, canvas.width, canvas.height);
 	
+	//put current position in projectile coord set
+	drawPoints[count] = [0,0];
+	drawPoints[count][0] = balls[0].x;
+	drawPoints[count][1] = balls[0].y;
+	count++;
+	
+	//loop through balls and draw
 	for (var i=0; i<balls.length; i++) {
 		if (i==0) {
 			context.fillStyle = "rgba(255,0,0,1)";
@@ -47,14 +54,42 @@ function animate() {
 		context.arc(balls[i].x, balls[i].y, balls[i].radius, 0, Math.PI*2, false);
 		context.closePath();
 		context.fill();
-		
 	};
-	if(playAnimation) {
-		setTimeout(animate, 33);
-	};
+
+	//put updated position in projectile array
+	drawPoints[count] = [0,0]
+	drawPoints[count][0] = balls[0].x;
+	drawPoints[count][1] = balls[0].y;
+	count++;
+
+	//draw projectile path from points
+	context.beginPath();
+	context.moveTo(drawPoints[0][0],drawPoints[0][1]);
+	for (var i = 1; i<count; i++) 
+		context.lineTo(drawPoints[i][0],drawPoints[i][1]);
+	context.stroke();
+
+	//if out of bounds, stop animation
 	if(balls[0].x < 75) {
 		playAnimation = false;
 	};
+
+	//if still playing, repeat
+	if(playAnimation) {
+		setTimeout(animate, 33);
+	};
 };
 
-animate();
+function playLoop() {
+	count = 0; // reset num points in projectile array
+	context.clearRect(0,0, canvas.width, canvas.height);
+	balls[0] = new Ball(422, 38, 10, -5, 0, .1);
+	balls[1] = new Ball(422, 38, 10, -5, 0, 0);
+	balls[2] = new Ball(422, 38, 10, 0, 0, .1);
+	
+	// set true and play
+	playAnimation = true;
+	animate();
+};
+
+animate(); //initial drawing
